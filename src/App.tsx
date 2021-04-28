97a68d4e192a5a9577bf6cfef738cb0bd42c9514
Dmitry Kelek <kelekofficial@gmail.com>
2021-04-28 14:17:56 +0300

    drawing tasks through the map and storing the tasks in the local state

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import {Todolist} from "./Components/Todolist";
 
+export type TaskType = {
+    id: number
+    title: string
+    isDone: boolean
+}
 function App() {
+
+    const [tasks, setTasks] = useState<TaskType[]>([
+        { id: 1, title: "HTML", isDone: true },
+        { id: 2, title: "CSS", isDone: true },
+        { id: 3, title: "JS", isDone: true },
+        { id: 4, title: "React", isDone: false },
+        { id: 5, title: "Redux", isDone: false }
+    ])
+
+
     return (
         <div className="App">
-            <Todolist />
+            <Todolist
+                title={"What to learn"}
+                tasks={tasks}
+            />
         </div>
     );
 }
--- src/Components/Todolist.tsx
+++ src/Components/Todolist.tsx
@@ -1,24 +1,29 @@
 import React from "react";
 import S from "./Todolist.module.css"
+import {TaskType} from "../App";
 
 type TodolistPropsType = {
-
+    title: string
+    tasks: TaskType[]
 }
 
 export function Todolist(props: TodolistPropsType) {
+    const tasks = props.tasks.map( t => <li key={t.id}>
+        <input
+            type="checkbox"
+            checked={t.isDone}
+        />
+        <span>{t.title}</span> </li>
+    )
     return (
         <div className={S.todolist}>
-            <h3>What to learn</h3>
+            <h3>{props.title}</h3>
             <div>
                 <input />
                 <button>+</button>
             </div>
             <ul>
-                <li> <input type="checkbox" checked={true}/> <span>HTML</span> </li>
-                <li> <input type="checkbox" checked={true}/> <span>CSS</span> </li>
-                <li> <input type="checkbox" checked={true}/> <span>JS</span> </li>
-                <li> <input type="checkbox" checked={false}/> <span>React</span> </li>
-                <li> <input type="checkbox" checked={false}/> <span>Redux</span> </li>
+                {tasks}
             </ul>
             <div>
                 <button>All</button>
