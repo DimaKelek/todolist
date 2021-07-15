c73bac3e640c1fde53fac51f9e9a57f386ab9223
Dmitry Kelek <kelekofficial@gmail.com>
2021-07-15 23:57:14 +0300

    Styles for login form has been added

--- src/Components/Login/Login.tsx
+++ src/Components/Login/Login.tsx
@@ -1,6 +1,23 @@
 import React from 'react'
-import {Checkbox, FormControl, FormControlLabel, FormGroup, FormLabel, TextField, Button, Grid} from '@material-ui/core'
+import {
+    Button,
+    Checkbox,
+    FormControl,
+    FormControlLabel,
+    FormGroup,
+    Grid,
+    IconButton,
+    TextField,
+    Tooltip
+} from '@material-ui/core'
 import {useFormik} from "formik";
+import {useDispatch, useSelector} from "react-redux";
+import {AppStateType} from "../../store/store";
+import {Redirect} from "react-router-dom";
+import {login} from "../../store/auth-reducer";
+import Paper from '@material-ui/core/Paper';
+import S from "./Login.module.css";
+import {Fingerprint, Info} from "@material-ui/icons";
 
 type FormikErrorType = {
     email?: string
@@ -8,6 +25,16 @@ type FormikErrorType = {
     rememberMe?: boolean
 }
 export const Login = () => {
+    const isLoggedIn = useSelector<AppStateType, boolean>(state => state.auth.isLoggedIn)
+    const dispatch = useDispatch()
+    const [open, setOpen] = React.useState(false);
+
+
+    const text = `Test Email: free@samuraijs.com Test Password: free`
+    const handleTooltip = () => {
+        setOpen(!open);
+    };
+
     const formik = useFormik({
         initialValues: {
             email: '',
@@ -17,59 +44,89 @@ export const Login = () => {
         validate: (values) => {
             const errors: FormikErrorType = {};
             if (!values.email) {
-                errors.email = 'Required';
+                errors.email = 'Email is equired';
             } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
                 errors.email = 'Invalid email address';
+            }
+            if (!values.password) {
+                errors.password = 'Password is equired';
+            } else if (values.password.length < 6) {
+                errors.password = 'Password has been 6+ symbols';
             }
             return errors;
         },
         onSubmit: values => {
-            alert(JSON.stringify(values));
+            dispatch(login(values))
             formik.resetForm()
         },
     })
 
-
-    const style = {
-        marginTop: "20px",
-        color: "#ffffff"
+    if (isLoggedIn) {
+        return <Redirect to={"/"}/>
     }
-
-    return <Grid container style={style}>
-        <Grid item xs={4}>
-            <form onSubmit={formik.handleSubmit}>
-                <FormControl>
-                    <FormLabel style={{color: "#ffffff"}}>
-                        <p>Test data:</p>
-                        <p>Email: free@samuraijs.com</p>
-                        <p>Password: free</p>
-                    </FormLabel>
-                    <FormGroup>
-                        <TextField
-                            label="Email"
-                            margin="normal"
-                            {...formik.getFieldProps("email")}
-                            onBlur={formik.handleBlur}
-                            helperText={formik.errors.email}
-                            error={formik.touched.email && !!formik.errors.email}
-                        />
-                        <TextField
-                            type="password"
-                            label="Password"
-                            margin="normal"
-                            {...formik.getFieldProps("password")}
-                        />
-                        <FormControlLabel
-                            label={'Remember me'}
-                            control={<Checkbox
-                                color={"primary"}
-                                {...formik.getFieldProps("rememberMe")}
-                            />}
-                        />
-                        <Button type={'submit'} variant={'contained'} color={'primary'}>Login</Button>
-                    </FormGroup>
-                </FormControl>
-            </form>
+    return (
+        <Grid container className={S.login_container}>
+            <Paper elevation={6} className={S.paper}>
+                <Grid item xs={4}>
+                    <div className={S.testData}>
+                        <Tooltip
+                            PopperProps={{
+                                disablePortal: true,
+                            }}
+                            open={open}
+                            disableFocusListener
+                            disableHoverListener
+                            disableTouchListener
+                            title={text}
+                            onBlur={handleTooltip}
+                            placement="right-start"
+                        >
+                            <IconButton onClick={handleTooltip}>
+                                <Info/>
+                            </IconButton>
+                        </Tooltip>
+                    </div>
+                    <form onSubmit={formik.handleSubmit} className={S.item}>
+                        <FormControl>
+                            <div className={S.title}>
+                                <h2>Login</h2>
+                                <Fingerprint/>
+                            </div>
+                            <FormGroup>
+                                <div className={S.field}>
+                                    <TextField
+                                        label="Email"
+                                        margin="normal"
+                                        {...formik.getFieldProps("email")}
+                                        onBlur={formik.handleBlur}
+                                        helperText={formik.errors.email}
+                                        error={formik.touched.email && !!formik.errors.email}
+                                    />
+                                </div>
+                                <div className={S.field}>
+                                    <TextField
+                                        type="password"
+                                        label="Password"
+                                        margin="normal"
+                                        {...formik.getFieldProps("password")}
+                                        helperText={formik.errors.password}
+                                        error={formik.touched.password && !!formik.errors.password}
+                                    />
+                                </div>
+                                <FormControlLabel
+                                    label={'Remember me'}
+                                    control={<Checkbox
+                                        color={"primary"}
+                                        {...formik.getFieldProps("rememberMe")}
+                                    />}
+                                    className={S.checkbox}
+                                />
+                                <Button type={'submit'} variant={'contained'} color={'primary'}>Login</Button>
+                            </FormGroup>
+                        </FormControl>
+                    </form>
+                </Grid>
+            </Paper>
         </Grid>
-    </Grid>
+    )
 }
